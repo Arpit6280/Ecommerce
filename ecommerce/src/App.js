e04fb157dc40d7814494d3881c1f46b22993e080
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2024-01-07 21:31:11 +0530

    Adding Login and SignUp only css

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,6 +5,8 @@ import Order from "./pages/order/Order";
 import Cart from "./pages/cart/Cart";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import Nopage from "./pages/nopage/Nopage";
+import Login from "./pages/registration/Login";
+import SignUp from "./pages/registration/SignUp";
 import MyState from "./context/data/myState";
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path="/order" element={<Order />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/*" element={<Nopage />} />
         </Routes>
       </BrowserRouter>
