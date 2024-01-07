ee8e25627f23a5e16ae3d870afae20743c2c7ff8
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2024-01-07 21:18:00 +0530

    creating navbar and footer

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
-
-// import MyState from "./context/data/myState";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Order from "./pages/order/Order";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import Nopage from "./pages/nopage/Nopage";
+import MyState from "./context/data/myState";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <MyState>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<Nopage />} />
+        </Routes>
+      </BrowserRouter>
+    </MyState>
+  );
 }
 
 export default App;
---
+++ ecommerce/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import Layout from "../../components/layout/Layout";
+import MyState from "../../context/data/myState";
+
+function Home() {
+  return <Layout></Layout>;
+}
+
+export default Home;
